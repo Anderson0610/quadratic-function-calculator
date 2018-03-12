3f017f910b542c104d5735e6843149880c5334cb
Anderson0610 <36825991+Anderson0610@users.noreply.github.com>
2018-03-12 11:11:49 -0700

    Add files via upload

---
+++ quadratic function calculator.py
@@ -0,0 +1,41 @@
+#quadratic function calculator
+git remote add origin git@github.com:Anderson0610/quadratic-function-calculator.git
+git push -u origin master
+import math
+from fractions import *
+
+a=float(input('a= '))
+b=float(input('b= '))
+c=float(input('c= '))
+
+user=input('If you want to get the answer in fraction, type yes, otherwise press any key'+'\n')
+if user=='yes':
+    fraction=True
+else:
+    fraction=False
+
+while a==0:
+    print('Not even a quadratic function, reteach your math please.')
+    a=float(input('a= '))
+    
+if b**2-4*a*c>0:
+    x1=(-b+math.sqrt(b**2-4*a*c))/2*a
+    x2=(-b-math.sqrt(b**2-4*a*c))/2*a
+    if fraction:
+        a=Fraction(x1)
+        b=Fraction(x2)
+        print('x1=',a)
+        print('x1=',b)
+    else:
+        print('x1=',x1)
+        print('x2=',x2)
+elif b**2-4*a*c==0:
+    x=(-b+math.sqrt(b**2-4*a*c))/2*a
+    if fraction:
+        a=Fraction(x)
+        print('x1=x2=',a)
+    else:
+        print('x1=x2=',x)
+    
+else:
+    print('no scolution')
